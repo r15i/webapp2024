0fa24b9da3ee34801d564f1df242b734d376eb47
r15i <risoebiso@gmail.com>
2024-03-03 13:38:21 +0100

    fixed_chown_issue_and_improved_hot_reload

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 echo need to add a variable for the position we want 
+echo starting the tomcat service 
 sudo ./utilities_linux/start_services.sh 
+echo starting browser
 google-chrome-stable http://localhost:8080/x/ > /dev/null &
+echo setting the aliases
 alias compile='/home/r15i/Scrivania/webapp2024/utilities_linux/autocopy.sh'
 
 
--- utilities_linux/autocopy.sh
+++ utilities_linux/autocopy.sh
@@ -1,6 +1,15 @@
 echo need to add a variable 
+echo stopping service 
+sudo systemctl stop tomcat10
+echo deleting old version
 sudo rm -rf /var/lib/tomcat10/webapps/x
+echo copy the new version 
 sudo cp -r /home/r15i/Scrivania/webapp2024/x /var/lib/tomcat10/webapps/x
+echo chainging the ownership to make it executable by the service
+sudo chown -R tomcat10 /var/lib/tomcat10/webapps/x/ 
+echo restarting the service 
 sudo systemctl restart tomcat10
+sleep 3 
+echo updating the screen
 xdotool search --onlyvisible --class Chrome windowfocus key ctrl+r
 
